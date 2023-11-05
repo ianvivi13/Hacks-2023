3bd81e00cf9f7c4928c7e7c1132e83c9c1f7dfe0
Gorgon954 <Mwilliams28@ycp.edu>
2023-11-04 22:57:48 -0400

    Happy paths and css

--- src/components/javascript/server.js
+++ src/components/javascript/server.js
@@ -2,7 +2,7 @@ var count = 0;
 
 function changeImg() {
     count++;
-    document.getElementById("problem").src = "../assets/created-problems/trolley-problem-" + count + ".jpg";
+    //document.getElementById("problem").src = "../assets/created-problems/trolley-problem-" + count + ".jpg";
     document.getElementById("no-lever").innerHTML = "0";
     document.getElementById("lever").innerHTML = "0";
 }
